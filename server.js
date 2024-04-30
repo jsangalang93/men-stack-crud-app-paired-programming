1d08e6ef72a06aad2d4326fcc5aee474a07208d3
jsangalang93 <jsangalang93@gmail.com>
2024-04-30 11:29:28 -0400

    CRD Working Need Edit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI,)
+
+const Car = require('./models/car');
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+res.render('home.ejs');
+})
+
+// --------------------------CREATE ROUTE--------------------------
+app.get("/cars/new", (req, res) => {
+    res.render('cars/new.ejs');
+})
+
+app.post('/cars', async (req, res) => {
+    console.log(req.body);
+    // const postedCars = 
+    await Car.create(req.body);
+    res.redirect('/cars');
+})
+// --------------------------END CREATE ROUTE--------------------------
+
+// -----------------------------READ ROUTE------------------------------
+
+app.get('/cars', async (req, res) => {
+        const allCars = await Car.find();
+        console.log(allCars);
+    res.render('cars/index.ejs', {cars: allCars});
+})
+
+app.get('/cars/:id', async (req, res) => {
+    const foundCar = await Car.findById(req.params.id);
+    res.render('cars/show.ejs', {car: foundCar});
+})
+
+// -----------------------------END READ ROUTE------------------------------
+
+// ------------------------------DELETE ROUTE------------------------------
+
+app.delete('/cars/:id', async (req, res) => {
+    await Car.findByIdAndDelete(req.params.id);
+    res.redirect("/cars");
+})
+
+// ---------------------------END DELETE ROUTE------------------------------
+
+// --------------------------------EDIT ROUTE-------------------------------
+
+
+
+// ------------------------------END EDIT ROUTE--------------------------------
+
+app.listen(3012, () => {
+    console.log('listening on 3012');
+})
